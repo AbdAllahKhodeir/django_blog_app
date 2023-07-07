fbf8a7723b8d2b6c4cf5a2682900e4c216f2bb98
AbdAllah Khodeir <abdallahkhodeir@gmail.com>
2023-07-06 20:27:26 -0600

    finalizing the app and fixing bugs

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 from django.urls import reverse
 
-
+# Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -19,17 +18,16 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs={'pk': self.pk})
+        return reverse("post_detail",kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
 
-
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey('blog.Post',related_name = 'comments',on_delete = models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
@@ -37,7 +35,7 @@ class Comment(models.Model):
         self.save()
 
     def get_absolute_url(self):
-        return reverse('post_list')
+        return reverse("post_list")
 
     def __str__(self):
         return self.text
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from blog.models import Post, Comment
 from django.utils import timezone
-from blog.forms import PostForm, CommentForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView,
                                   UpdateView, DeleteView)
+from blog.models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
-
 class AboutView(TemplateView):
     template_name = 'about.html'
 
@@ -47,12 +46,12 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
 
 
 class DraftListView(LoginRequiredMixin, ListView):
-    login_url = '/login/'
+    login_url = '/login'
     redirect_field_name = 'blog/post_list.html'
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
 
 
 @login_required
@@ -73,7 +72,7 @@ def add_comment_to_post(request, pk):
             comment.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = CommentForm()
+        form = CommentForm
     return render(request, 'blog/comment_form.html', {'form': form})
 
 
